d873c12599a0cb852774e9856114ca3fe64ca7da
Jazmingv <jaz.vainman@gmail.com>
2022-05-28 12:49:05 -0300

    ejercicio-10

---
+++ ejercicio-clase-10/index.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const multer  = require('multer');
+const container = require("./container.js");
+
+const app = express();
+const port = 3000;
+
+//set views & engine. I prefer ejs because it gives me more freedom when coding inside the layout
+app.set("views", "./ejs_views");
+app.set("view engine", "ejs");
+
+//midlewares
+app.use(express.urlencoded({ extended: true }));
+
+const uploads = multer({ dest: 'uploads/' });
+
+app.get("/data", (req, res) => {
+    const container = new Container();
+    const listadoProductos = container.getAll();
+    res.render("data", { listadoProductos });
+});
+
+app.post("/form", upload.single('thumbnail') , (req, res) => {
+    const { title, author, price } = req.body;
+    const { thumbnail } = req.file;
+    productsList.push({ title, author, price, thumbnail });
+    res.redirect("/data");
+});
+
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
+});
--- ejercicio-clase-8/server.js
+++ ejercicio-clase-8/server.js
@@ -1,19 +1,19 @@
 const express = require("express");
-
-const app = express();
-const port = 3000;
-const router = express.Router();
-
 const container = require("./container.js");
 
+const app = express();
+const router = express.Router();
+const port = 3000;
+
+//midlewares
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use("/api/productos", router);
-app.use('/',express.static("public"));
+app.use('/',express.static("upload"));
 
 const server = app.listen(port, () => {
-  console.log("Servidor ON!");
+  console.log("I'm listening...");
 });
 
 server.on("error", (error) => console.log(`Error en el servidor ${error}`));
@@ -28,7 +28,7 @@ router.get("/:id", (req, res) => {
   const id = parseInt(req.params.id);
   const container = new Container();
   const productByID = container.getByID(id);
-  if (productByID[0] != undefined) {
+  if (productByID != undefined) {
     res.send({ producto: productByID });
   } else res.status(404).send({ error: "Producto no encontrado" });
 });
@@ -45,7 +45,7 @@ router.put("/:id", (req, res) => {
   const product = req.body;
   const container = new Container();
   const putProduct = container.update(id, product);
-  if (putProduct[0] != undefined) {
+  if (putProduct != undefined) {
     res.send({ producto: putProduct });
   } else res.status(404).send({ error: "Producto no encontrado" });
 });
@@ -54,7 +54,7 @@ router.delete("/:id", (req, res) => {
   const id = parseInt(req.params.id);
   const container = new Container();
   const deleteProduct = container.delete(id);
-  if (deleteProduct[0] != undefined) {
+  if (deleteProduct != undefined) {
     res.status(200).send({ mensaje: "producto eliminado" });
   } else res.status(404).send({ error: "Producto no encontrado" });
 });
